6c0fd14d13726dd66edc1e66cc292f235ada04c0
patelabhi1607 <abhipatel1698@gmail.com>
2023-03-08 11:01:41 +0530

    Minor changes.

--- spotify/util.py
+++ spotify/util.py
@@ -16,7 +16,9 @@ def get_user_tokens(session_id):
         return None
 
 
-def update_or_create_user_tokens(session_id, access_token, token_type, expires_in, refresh_token):
+def update_or_create_user_tokens(
+    session_id, access_token, token_type, expires_in, refresh_token
+):
     tokens = get_user_tokens(session_id)
     expires_in = timezone.now() + timedelta(seconds=expires_in)
 
@@ -25,11 +27,15 @@ def update_or_create_user_tokens(session_id, access_token, token_type, expires_i
         tokens.refresh_token = refresh_token
         tokens.expires_in = expires_in
         tokens.token_type = token_type
-        tokens.save(update_fields=['access_token',
-                                   'refresh_token', 'expires_in', 'token_type'])
+        tokens.save(update_fields=["access_token", "expires_in", "token_type"])
     else:
-        tokens = SpotifyToken(user=session_id, access_token=access_token,
-                              refresh_token=refresh_token, token_type=token_type, expires_in=expires_in)
+        tokens = SpotifyToken(
+            user=session_id,
+            access_token=access_token,
+            refresh_token=refresh_token,
+            token_type=token_type,
+            expires_in=expires_in,
+        )
         tokens.save()
 
 
@@ -48,17 +54,21 @@ def is_spotify_authenticated(session_id):
 def refresh_spotify_token(session_id):
     refresh_token = get_user_tokens(session_id).refresh_token
 
-    response = post('https://accounts.spotify.com/api/token', data={
-        'grant_type': 'refresh_token',
-        'refresh_token': refresh_token,
-        'client_id': CLIENT_ID,
-        'client_secret': CLIENT_SECRET
-    }).json()
+    response = post(
+        "https://accounts.spotify.com/api/token",
+        data={
+            "grant_type": "refresh_token",
+            "refresh_token": refresh_token,
+            "client_id": CLIENT_ID,
+            "client_secret": CLIENT_SECRET,
+        },
+    ).json()
 
-    access_token = response.get('access_token')
-    token_type = response.get('token_type')
-    expires_in = response.get('expires_in')
-    refresh_token = response.get('refresh_token')
+    access_token = response.get("access_token")
+    token_type = response.get("token_type")
+    expires_in = response.get("expires_in")
+    refresh_token = response.get("refresh_token")
 
     update_or_create_user_tokens(
-        session_id, access_token, token_type, expires_in, refresh_token)
+        session_id, access_token, token_type, expires_in, refresh_token
+    )
